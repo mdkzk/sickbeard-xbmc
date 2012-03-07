464272f0ff8a4f9e9c7cde9f937c81aa44fef119
zachmoore1@gmail.com <zachmoore1@gmail.com@b2323b8b-80c0-bc96-62dd-fecc1db4929c>
2012-03-07 02:34:00 +0000

    Added support for Sickbeard being password protected.

--- trunk/plugin.program.sickbeard/resources/lib/settings.py
+++ trunk/plugin.program.sickbeard/resources/lib/settings.py
@@ -1,14 +1,29 @@
 import xbmcaddon
 import os
 import urllib
+import urllib2
+import xbmcgui
 
 # Hackish... not sure if there is a better way to get the API key
 # Parses the HTML of the General page and pulls the API key
-def GetAPIKey(ip, port):
+def GetAPIKey(ip, port, username, password):
     # Get API key from Sickbeard
-    html=urllib.urlopen('http://'+ip+':'+port+'/config/general/')
-    result=html.readlines()
-    html.close()
+    baseurl = "http://" + ip + ":" + port
+    if username and password:
+        password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+        url = "http://" + ip + ":" + port+ '/config/general/'
+        password_manager.add_password(None, url, username, password)
+        authhandler = urllib2.HTTPBasicAuthHandler(password_manager)
+        opener = urllib2.build_opener(authhandler)
+        urllib2.install_opener(opener)
+        req = urllib2.Request(url)
+        result = urllib2.urlopen(req)
+        result = result.readlines()
+    else:    
+        html=urllib.urlopen('http://'+ip+':'+port+'/config/general/')
+        result=html.readlines()
+        html.close()
+
     api_line = ""
     for line in result:
       if "id=\"api_key\"" in str(line):
@@ -21,5 +36,14 @@ def GetAPIKey(ip, port):
 __addon__ = xbmcaddon.Addon(id='plugin.program.sickbeard')
 __ip__ = __addon__.getSetting('Sickbeard IP')
 __port__= __addon__.getSetting('Sickbeard Port')
-__APIKey__ = GetAPIKey(__ip__, __port__)
+__username__ = __addon__.getSetting('Sickbeard Username')
+__password__= __addon__.getSetting('Sickbeard Password')
+
+try:
+    __APIKey__ = GetAPIKey(__ip__, __port__, __username__,__password__)
+except:
+    dialog = xbmcgui.Dialog()
+    dialog.ok("Sickbeard Error", "Check Username and Password Settings")
+    sys.exit()
+
 __url__='http://'+__ip__+':'+__port__+'/api/'+__APIKey__+'/'
